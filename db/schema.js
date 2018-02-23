9abec4fde330c0dcfe072cd027678c1dd768c471
Johnny Siu <siujohnny@gmail.com>
2018-02-22 17:03:07 -0800

    [Fix] - merge

--- db/schema.js
+++ db/schema.js
@@ -1,8 +1,16 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+const connection = new Sequelize('insta', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
 
+  pool: {
+    max: 20,
+    min: 0
 
+  }
+});
 
-const Users = sequelize.define('users', {
+const Users = connection.define('users', {
   id: {
     type: Sequelize.INTEGER,
     allowNull: false,
@@ -29,3 +37,124 @@ const Users = sequelize.define('users', {
   }
 });
 
+const Submissions = connection.define('submissions', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  image_url: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  caption: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  like_count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Users,
+    key: 'id',
+    }
+  }
+  
+});
+
+const Comments = connection.define('comments', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  submission_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Submissions,
+      key: 'id',
+      }
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: { model: Users,
+      key: 'id',
+      }
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  
+});
+
+const Likes = connection.define('likes', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  submission_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Submissions,
+      key: 'id',
+      }
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Users,
+      key: 'id',
+      }
+  },
+  
+});
+const Followers = connection.define('followers', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Users,
+      key: 'id',
+      }
+  },
+  follower_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: { model: Users,
+      key: 'id',
+      }
+  },
+  
+});
+
+connection.sync().then(function(){
+  Users.create({
+
+    name: 'johnny',
+    username: 'johnny',
+    gender: 'M',
+    profile_pic: 'google.com',
+    bio: 'cool',
+    password: '123'
+  })
+});
+
+module.exports = connection;
