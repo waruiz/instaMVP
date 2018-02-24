be39eb29e3e02102741c49f87ce45d88b6310304
Nelson Chen <nelsonchen5@gmail.com>
2018-02-24 14:32:35 -0800

    rebase hell

--- server/models/nelson.js
+++ server/models/nelson.js
@@ -1,16 +1,75 @@
 //nelson file
 
-const db = require('../../db/schema')
-
+const db = require("../../db/schema");
 
 const getAllSubs = () => {
-  return db.Submissions.findAll()
-}
+  return db.Submissions.findAll();
+};
 
+const deleteSubmission = req => {
+  return db.Comments.destroy({
+    where: {
+      submission_id: req.body.id
+    }
+  })
+    .then(results => {
+      return db.Likes.destroy({
+        where: {
+          submission_id: req.body.id
+        }
+      });
+    })
+    .then(data => {
+      return db.Submissions.destroy({
+        where: {
+          id: req.body.id
+        }
+      });
+    });
+};
 
-
-// const getFollowing = () =>{
-//   return db.Followers.findAll({host_id})
-// }
-
+const deleteUser = req => {
+  return db.Comments.destroy({
+    where: {
+      user_id: req.body.id
+    }
+  })
+    .then(results => {
+      return db.Likes.destroy({
+        where: {
+          user_id: req.body.id
+        }
+      });
+    })
+    .then(data => {
+      return db.Submissions.destroy({
+        where: {
+          user_id: req.body.id
+        }
+      });
+    })
+    .then(data => {
+      return db.Followers.destroy({
+        where: {
+          follower_id: req.body.id
+        }
+      });
+    })
+    .then(data => {
+      return db.Followers.destroy({
+        where: {
+          host_id: req.body.id
+        }
+      });
+    })
+    .then(data => {
+      return db.Users.destroy({
+        where: {
+          id: req.body.id
+        }
+      });
+    });
+};
 module.exports.getAllSubs = getAllSubs;
+module.exports.deleteSubmission = deleteSubmission;
+module.exports.deleteUser = deleteUser;
