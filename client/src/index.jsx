45fd5e55aeb75540eb68daaf5aa00e3a62ba875a
Will Ruiz <thisis.willruiz@gmail.com>
2018-02-26 18:01:29 -0800

    Review merge conflicts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,48 +1,30 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+import App from "./components/app.jsx";
+
 import LandingPage from './components/LandingPage.jsx';
 import Navbar from './components/Home/Navbar.jsx'
-
 import CommentList from './components/Home/CommentList.jsx'
+
+const store = createStore(allReducers);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render(props) {
-    return (<div>
-
-
-
-      <div align="right">
-   <Navbar />
-      </div>
-
-      <div>
-   <LandingPage />
-      </div>
-
-      <div>
-    <CommentList />
-      </div>
-
-
-    </div>);
+    return (
+    <div>
+      <div align="right"><Navbar /></div>
+      <div><LandingPage /></div>
+      <div><CommentList /></div>
+    </div>
+    );
   }
 }
-
-ReactDOM.render(<App/>, document.getElementById('app'));
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-// import LandingPage from "./components/LandingPage.jsx";
-import allReducers from "./reducers";
-import { Provider } from "react-redux";
-import App from "./components/app.jsx";
-
-const store = createStore(allReducers);
 
 ReactDOM.render(
   <Provider store = {store}>
@@ -50,4 +32,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("app")
 );
->>>>>>> boilerplate for redux
