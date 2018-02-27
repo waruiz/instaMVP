2ef32fc6d06ca0616984ea3949391a0ed92f5a06
Johnny Siu <siujohnny@gmail.com>
2018-02-27 13:31:20 -0800

    removing folder

--- client/src/johnny/actions/show-followers.js
+++
@@ -1,7 +0,0 @@
-export const showFollowers = (followerList) => {
-  console.log('You clicked show followers')
-  return {
-    type: 'SHOW_FOLLOWERS',
-    payload: followerList // do we need payload?
-  }
-}
