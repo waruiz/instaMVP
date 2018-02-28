4fc86f604513661c2f7dc586bf1a9762526ff0b5
Will Ruiz <thisis.willruiz@gmail.com>
2018-02-28 12:23:42 -0800

    Create smart component for user posts

---
+++ client/src/components/User/PostsContainer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import {connect} from 'react-redux'
+import actions from "../../Redux/actions/index";
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateUserPosts: posts => dispatch(actions.updateUserPosts(posts))
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    userPostsState: state.userPostsState
+  };
+};
+
+class PostsContainer extends React.Component {
+  constructor (props) {
+    super (props);
+    this.getUserPosts = this.getUserPosts.bind(this);
+  }
+
+  getUserPosts () {
+    axios.get(`/subs/4`)
+      .then(result => {
+        console.log(this.props);
+        this.props.updateUserPosts(result.data);
+      })
+  }
+
+  render () {
+    return (
+      <div id="posts-container">
+        <h1 onClick={this.getUserPosts}>Posts</h1>
+        {this.props.userPostsState.map((post, i) => {
+          return (
+          <div key ={i}>
+            {post.image_url}
+          </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+const PostsContainerPage = connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
+export default PostsContainerPage;
