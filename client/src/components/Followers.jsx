45674be102af3b92d3bed83df2f0c11066e48d79
Michael Wachell <wachell@gmail.com>
2018-02-28 20:28:17 -0800

    followerslightbox added

--- client/src/components/Followers.jsx
+++ client/src/components/Followers.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import actions from "../Redux/actions/index";
 
 const mapDispatchToProps = dispatch => {
@@ -11,39 +11,46 @@ const mapDispatchToProps = dispatch => {
 };
 
 const mapStateToProps = state => {
-  return {
-    followersState: state.followersState
-  };
+  return {followersState: state.followersState};
 };
 
 class Followers extends React.Component {
   constructor(props) {
     super(props);
+    this.showFollowers = this.showFollowers.bind(this)
   }
 
   showFollowers() {
     //should pull current user from state later on
-    axios.get('/followers/thejhnny')
-      .then((response) => {
-        console.log('THIS IS THE RESPONSE ', response.data);
-        console.log('this is our props ', this.props.updateFollowers);
-        this.props.updateFollowers(response.data);
-        console.log(this.props)
-      })
-      .catch((error) => {
-        console.log('ERROR IS: ', error);
-      })
+    axios.get('/followers/thejhnny').then((response) => {
+      console.log('THIS IS THE RESPONSE ', response.data);
+      console.log('this is our props ', this.props.updateFollowers);
+      this.props.updateFollowers(response.data);
+      console.log(this.props)
+    }).catch((error) => {
+      console.log('ERROR IS: ', error);
+    })
   }
 
   render() {
-    return (
-      <div>
-        <h2>FOLLOWERS</h2>
-        {this.props.followersState.map( (item, i) => {
-          return(<div key={i}>{item.username}</div>)
-        })}
+    return (<div>
+
+      <button className="btn btn-primary" data-toggle="modal" data-target=".followers-list" onClick={this.showFollowers}>Followers</button>
+
+      <div className="modal fade followers-list" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-sm">
+          <h5>Followers</h5>
+          <div className="modal-content">
+            {
+              this.props.followersState.map((item, i) => {
+                return (<div key={i}>{item.username}</div>)
+              })
+            }
+          </div>
+        </div>
       </div>
-    )
+
+    </div>)
   }
 }
 
