294788f1429b5da714469e2d0a5f00bd1db3caea
Will Ruiz <thisis.willruiz@gmail.com>
2018-02-28 17:48:24 -0800

    Render and POST friend request

--- client/src/components/Home/Requests.jsx
+++ client/src/components/Home/Requests.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import actions from '../../Redux/actions/';
 
-const Requests = () => (
-  <div></div>
-);
+const mapDispatchToProps = dispatch => {
+  return {
+    updateAddRequestState: (username) => dispatch(actions.updateAddRequestState(username))
+  }
+};
 
-export default Requests;
+const mapStateToProps = state => {
+  return {
+    addRequestState: state.addRequestState
+  }
+};
+
+class Requests extends React.Component {
+  constructor (props) {
+    super (props);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.postRequest = this.postRequest.bind(this);
+  }
+  onUsernameChange (e) {
+    this.props.addRequestState.username = e.target.value;
+  }
+  postRequest () {
+    axios
+      .post('/requestfollower', {
+        host: `Will`,
+        username: this.props.addRequestState.username
+      })
+      .then(result => {
+        console.log('Successful friend request: ', result);
+      })
+      .catch(err => {
+        console.log('Failed friend request: ', err);
+      })
+  }
+  render () {
+    return (
+      <div>
+        <form>
+        <input type="text" name="username" placeholder="Username" autoComplete="off" onChange={this.onUsernameChange} />
+        <input type="submit" value="Follow" onClick={() =>{this.postRequest()}} />
+        </form>
+      </div>
+    );
+  }
+}
+
+const RequestsContainer = connect(mapStateToProps, mapDispatchToProps)(Requests);
+export default RequestsContainer;
