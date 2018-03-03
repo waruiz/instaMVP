cbecbf080448a17b2524c066c2d77715bde7c05b
Michael Wachell <wachell@gmail.com>
2018-03-02 17:41:31 -0800

    fixed dupe comments

--- client/src/Redux/reducers/reducer-postComments.js
+++ client/src/Redux/reducers/reducer-postComments.js
@@ -3,8 +3,11 @@ export default function postCommentState(state = [], action) {
   switch (action.type) {
     case 'UPDATE_POST_COMMENTS':
       if (action.payload.data) {
-          state.push(action.payload.data)
+        state.push(action.payload.data)
         return state;
+      } else if (action.payload === "initialize") {
+          state = [];
+          return state;
       } else {
         return state = state.concat(action.payload);
       }
